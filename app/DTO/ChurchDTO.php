f45e1989799906b84503472962443cfe161b199b
Danilo Ganda <daniloganda95@gmail.com>
2023-11-27 15:36:14 -0300

    feat: :sparkles: Create ChurchDTO to representation data Church
    include property to the Church and facility in transfer of data at system

---
+++ app/DTO/ChurchDTO.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace App\DTO;
+
+class ChurchDTO extends AbstractDTO {
+
+    public function __construct(
+        public readonly string $name,
+        public readonly string $email,
+        public readonly string $address,
+        public readonly string $cnpj,
+        public readonly string $UF,
+        public readonly string $date_inauguration
+    ) 
+    {
+    } 
+}
