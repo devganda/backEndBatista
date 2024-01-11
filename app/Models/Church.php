092a23baa38244deed4a6701c219a01c99bfae92
Danilo Ganda <daniloganda95@gmail.com>
2024-01-11 15:38:20 -0300

    feat(sparkles): :sparkles: add interface Church

--- app/Models/Church.php
+++ app/Models/Church.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\hasMany;
 
 class Church extends Model
 {
@@ -12,13 +13,13 @@ class Church extends Model
     protected $table = "churchs";
 
     protected $fillable = ['name', 'email', 'cnpj', 'address', 'UF', 'date_inauguration'];
-    
+
     /**
      * Um modelo Church que tem muitos membros (um-para-muitos)
-     * @return \Illuminate\Database\Eloquent\Relations\hasMany
      */
-    public function members()
+    public function members():hasMany
     {
         return $this->hasMany(Member::class, 'church_id', 'id');
     }
+
 }
