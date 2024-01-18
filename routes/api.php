4bf3b70cce15e6420581dfe0c0a3d4e0d2875ec3
Danilo Ganda <daniloganda95@gmail.com>
2024-01-18 12:21:48 -0300

    feat(sparkles): :sparkles: create route login and add routes in middleware auth

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ChurchController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MemberController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,20 +18,23 @@ use App\Http\Controllers\MemberController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [UserController::class, 'create']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/church', [ChurchController::class, 'index']);
+    Route::get('/church/edit/{id}', [ChurchController::class, 'edit']);
+    Route::post('/church/create', [ChurchController::class, 'create']);
+    Route::put('/church/update/{id}', [ChurchController::class, 'update']);
+    Route::delete('/church/delete/{id}', [ChurchController::class, 'delete']);
+
+    Route::get('/members', [MemberController::class, 'index']);
+    Route::post('/members/create', [MemberController::class, 'create']);
+    Route::get('/members/edit/{id}', [MemberController::class, 'edit']);
+    Route::get('/members/show/{id}', [MemberController::class, 'findMembersByChurchID']);
+    Route::put('/members/update/{id}', [MemberController::class, 'update']);
+    Route::delete('/members/delete/{id}', [MemberController::class, 'delete']);
 });
 
-Route::get('/church', [ChurchController::class, 'index']);
-Route::get('/church/edit/{id}', [ChurchController::class, 'edit']);
-Route::post('/church/create', [ChurchController::class, 'create']);
-Route::put('/church/update/{id}', [ChurchController::class, 'update']);
-Route::delete('/church/delete/{id}', [ChurchController::class, 'delete']);
 
-Route::get('/members', [MemberController::class, 'index']);
-Route::post('/members/create', [MemberController::class, 'create']);
-Route::get('/members/edit/{id}', [MemberController::class, 'edit']);
-Route::get('/members/show/{id}', [MemberController::class, 'findMembersByChurchID']);
-Route::put('/members/update/{id}', [MemberController::class, 'update']);
-Route::delete('/members/delete/{id}', [MemberController::class, 'delete']);
 
