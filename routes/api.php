7a8627890f9f958b5f68d9e178297266eb2a4538
Danilo Ganda <daniloganda95@gmail.com>
2024-01-24 16:12:10 -0300

    feat(sparkles): :sparkles: add middlewares authenticate and permissions

--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,27 @@
 <?php
 
 use App\Http\Controllers\ChurchController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::post('/register', [UserController::class, 'create']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/edit/{id}', [UserController::class, 'edit']);
+    Route::put('/users/update/{id}', [UserController::class, 'update']);
+    Route::delete('/users/delete/{id}', [UserController::class, 'delete']);
+
     Route::get('/church', [ChurchController::class, 'index']);
     Route::get('/church/edit/{id}', [ChurchController::class, 'edit']);
-    Route::post('/church/create', [ChurchController::class, 'create']);
+    Route::post('/church/create', [ChurchController::class, 'create'])
+        ->middleware('checkMultiplePermissions:admin,basic');
     Route::put('/church/update/{id}', [ChurchController::class, 'update']);
     Route::delete('/church/delete/{id}', [ChurchController::class, 'delete']);
 
