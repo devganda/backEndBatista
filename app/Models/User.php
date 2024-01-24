3282519e0aaa4b02f5252d8d1b01ab98d6984201
Danilo Ganda <daniloganda95@gmail.com>
2024-01-24 16:11:34 -0300

    feat(sparkles): :sparkles: add relations with church and permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -21,6 +23,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'church_id'
     ];
 
     /**
@@ -42,4 +45,41 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function church():BelongsTo
+    {
+        return $this->belongsTo(Church::class);
+    }
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function hasAnyPermission($permissions):bool
+    {
+        var_dump($permissions);
+        return $this->permissions()->whereIn('name', $permissions)->count() > 0;
+    }
+
+    public function assignPermission(string $permission): void
+    {
+        $this->permissions()->firstOrCreate([
+            'name' => $permission,
+        ]);
+    }
+
+    public function hasPermission(string $permission): bool
+    {
+        return $this->permissions()->where('name', $permission)->exists();
+    }
+
+    public function removePermission(string $permission):void
+    {
+       $permission = $this->permissions()->where('name', $permission)->first();
+
+       if($permission){
+            $this->permissions()->detach($permission);
+       }
+    }
 }
