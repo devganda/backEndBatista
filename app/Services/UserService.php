fbb82e37e752542f89fa5409939152e7d9c638ad
Danilo Ganda <daniloganda95@gmail.com>
2024-01-25 17:56:36 -0300

    feat(sparkles): :sparkles: change request rules

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,10 +2,11 @@
 
 namespace App\Services;
 
-
 use App\DTO\UserDTO;
+use App\Http\Requests\UserRequest;
+use App\Http\Requests\UserRequestUpdate;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Exception;
 
 class UserService
 {
@@ -17,59 +18,73 @@ class UserService
         return $this->result;
     }
 
-    public function create(Request $request):array
+    public function create(UserRequest $request):array
     {
-        $permission = $request->input('permission');
-        $userModel = $this->getUserModel($request);
-        $userModel->save();
-        $userModel->assignPermission($permission);
+        try{
+            $permission = $request->input('permission');
+            $userModel = $this->getUserModel($request);
+            $userModel->save();
+            $userModel->assignPermission($permission);
+            $this->result['message'] = "Usuário criado com sucesso!";
+            return $this->result;
 
-        $this->result['message'] = "Usuário criado com sucesso!";
-
-        return $this->result;
+        }catch (Exception $e){
+            $this->result['error'] = "Error ao criar o usuário!";
+            return $this->result;
+        }
     }
 
     public function edit(string $id):array
     {
-        $user = User::find($id);
-        $church = $user->church;
-        $this->result['user'] = $user;
+        try{
+            $user = User::findOrFail($id);
+            $church = $user->church;
+            $permissions = $user->permissions;
+            $this->result['user'] = $user;
+            return $this->result;
 
-        return $this->result;
+        }catch (Exception $e){
+            $this->result['error'] = "Error ao buscar o usuário!";
+            return $this->result;
+        }
     }
 
-    public function update(Request $request, string $id):array
+    public function update(UserRequestUpdate $request, string $id):array
     {
-        $user = User::find($id);
+        try{
+            $user = User::findOrFail($id);
+            $permission = $request->input('permission');
+            $user->name =  $request->input('name');
+            $user->church_id =  $request->input('church_id');
+            $user->email =  $request->input('email');
+            $user->save();
+            $user->assignPermission($permission);
 
-        if(!$user){
-           $this->result['error'] = 'Usuário não encontrado!';
-           return $this->result;
+            $this->result['message'] = "Usuário editado com sucesso!";
+            $this->result['user'] = $user->find($id);
+            return $this->result;
+
+        }catch (Exception $e){
+            $this->result['error'] = "Error ao atualizar o usuário!";
+            return $this->result;
         }
-
-        $userModel = $this->getUserModel($request);
-        $userModel->update();
-
-        $this->result['message'] = "Usuário editado com sucesso!";
-        $this->result['user'] = $userModel->find($id);
-
-        return $this->result;
     }
 
     public function delete(string $id):array
     {
-        $user = User::find($id);
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+            $this->result['message'] = "Usuário excluído com sucesso!";
+            return $this->result;
 
-        if(!$user){
+        }catch (Exception $e){
             $this->result['error'] = "Usuário não encontrado!";
             return $this->result;
         }
-
-        $this->result['message'] = "Usuário excluído com sucesso!";
-        return $this->result;
     }
 
-    public function getUserModel(Request $request): User
+    public function getUserModel($request): User
     {
         $name = $request->input('name');
         $church_id = $request->input('church_id');
