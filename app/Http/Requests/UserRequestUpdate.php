7a48338c121375a4b5f47b523e0a195fe9ec7eb7
Danilo Ganda <daniloganda95@gmail.com>
2024-01-24 22:21:53 -0300

    feat(sparkles): :sparkles: add UserRequestUpdate

---
+++ app/Http/Requests/UserRequestUpdate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequestUpdate extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'church_id' => 'required|int',
+            'email' => 'required|email',
+            'password' => 'required|string',
+            'permission' => 'required|string',
+        ];
+    }
+}
