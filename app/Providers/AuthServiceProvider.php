ba87f4bf09f425bcbeafea3bd64f7909b38c6a51
Danilo Ganda <daniloganda95@gmail.com>
2024-01-25 17:57:01 -0300

    feat(sparkles): :sparkles: add gate master

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -22,6 +22,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Gate::define('master', function (User $user) {
+            return $user->hasPermission('master');
+        });
+
         Gate::define('admin', function (User $user) {
             return $user->hasPermission('admin');
         });
